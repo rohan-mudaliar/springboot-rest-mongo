fb29baf827ea5ee63f81f3fe392c11458b41c3ca
Rohan G <rohan.g@fptechscience.com>
2018-10-11 14:55:14 +0530

    first commit

---
+++ src/main/java/com/test/springDemo/SpringBoot_Rest_Mongo/SpringBootRestMongoApplication.java
@@ -0,0 +1,12 @@
+package com.test.springDemo.SpringBoot_Rest_Mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/springDemo/SpringBoot_Rest_Mongo/SpringBootRestMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.springDemo.SpringBoot_Rest_Mongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
